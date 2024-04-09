31b0a0dc8c5e067f3d54fe070c795926c11be90d
yash <yashraj.se10@gmail.com>
2024-04-09 13:33:35 +0530

    solana done

---
+++ client/next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "res-console.cloudinary.com",
+      "thelogicalindian.com",
+    ],
+  },
+};
+
+export default nextConfig;
